e2f12af42c0ff5acd81c2de1ff209d8329ec775f
walacefex <walacefex01@gmail.com>
2024-08-08 10:39:55 -0300

    chore: add git hooks setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "./_components/ui/button";
-
+import { Button } from "./_components/ui/button"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Click me</Button>
     </main>
-  );
+  )
 }
